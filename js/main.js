bacdbca8691b10c320543afa2bf188fcbbf8a2ad
Fabian Medina <97wifameki@gmail.com>
2024-05-04 13:51:44 -0500

    imcompleto

--- js/main.js
+++ js/main.js
@@ -48,67 +48,26 @@ class Carrito {
     };
 }
 
-// Agrego 5 productos de la clase Producto
-const producto1 = new Producto(1, "Camisa", 50000, 1, "Camisas");
-const producto2 = new Producto(2, "Pantalón", 70000, 2, "Jeans");
-const producto3 = new Producto(3, "Chaqueta", 100000, 2, "Chaquetas");
-const producto4 = new Producto(4, "Camiseta", 60000, 3, "Camisas");
-const producto5 = new Producto(5, "Short", 40000, 5, "Jeans");
-
 // Instancio un nuevo carrito
 const carrito = new Carrito();
 
-// Agrego los productos al carrito
-carrito.agregarProducto(producto1);
-carrito.agregarProducto(producto2);
-carrito.agregarProducto(producto3);
-carrito.agregarProducto(producto4);
-carrito.agregarProducto(producto5);
+//Eventos
+let botonMas = document.querySelectorAll("div .btn-mas");
+// for (const indice of botonMas){
+//     console.log(indice.innerText);
+// }
+let numero = parseInt(document.querySelectorAll("div .cantidad").value);
+botonMas.addEventListener("click", () =>{
+    numero++;
+    document.querySelectorAll("div .cantidad").value = numero;
+});
 
-console.log("Codigo Nombre " + "  " + " Precio")
-// Muestra los productos en consola
-carrito.mostrarProductos();
+let botonMenos = document.querySelectorAll("div .btn-menos");
+botonMenos.addEventListener("click", () =>{
+    numero--;
+    document.querySelectorAll("div .cantidad").value = numero;
+});
 
-// Pide al usuario ingresar la cantidad de productos a comprar
-let cantidadProductos = parseInt(prompt("Digite cuantos de los productos de la lista que se muestra por consola desea comprar"));
 
-// Valida la información ingresada por el usuario "cantidadProductos"
-if (!isNaN(cantidadProductos) && cantidadProductos > 0 && cantidadProductos <= 5) {
-    let totalPrecio = 0; // Variable para almacenar el precio total de los productos seleccionados
 
-    // for para solicitar y calcular el precio de los productos seleccionados
-    for (let i = 0; i < cantidadProductos; i++) {
-        let codigoProducto = parseInt(prompt("Ingrese el código del producto " + (i + 1) + " que desea comprar"));
-
-        // Validar que el código de producto esté en el rango válido (1-5)
-        if (codigoProducto >= 1 && codigoProducto <= 5) {
-            let cantidadProducto = parseInt(prompt("Ingrese la cantidad del producto con código " + codigoProducto + " que desea comprar"));
-
-            // let prueba = carrito.productos.forEach(indice => {
-            //     console.log(indice.nombre + "---" + indice.categoria);
-            // })
-
-            // Buscar el producto en el carrito por su código
-            let productoSeleccionado = carrito.productos.find
-            (producto => producto.codigo === codigoProducto);
-            // Si se encuentra el producto y la cantidad es válida, calcular su precio y sumarlo al total
-
-            if (productoSeleccionado && cantidadProducto > 0) {
-                totalPrecio += productoSeleccionado.precio * cantidadProducto;
-            } else {
-                alert("Cantidad incorrecta para el producto seleccionado.");
-                break;
-            }
-        } else {
-            alert("Código de producto incorrecto.");
-            break;
-        }
-    }
-
-    // Mostrar el precio total de los productos seleccionados
-    alert("El precio neto a pagar por los " + cantidadProductos + " productos es de $" + totalPrecio + " mil  pesos colombianos");
-    alert("¡Compra realida correctamente!")
-} else {
-    alert("Cantidad de productos incorrecta. Solo puede comprar entre 1 y 5 productos.");
-}
 
---
+++ js/pruebas.js
@@ -0,0 +1,19 @@
+const nombres = document.querySelectorAll("div .card-title");
+const precios = document.querySelectorAll("div .precios__normal");
+const cantidades = document.querySelectorAll("div .cantidad");
+// for (const cantidad of cantidades){
+//     console.log(cantidad.innerText);
+// }
+for (const indice of nombres){
+    console.log(indice.innerText);
+}
+for (const precio of precios){
+    console.log(precio.innerText);
+}
+cantidades.forEach(cantidad => {
+    console.log(cantidad.value);
+});
+
+
+
+
